4d1dbe47e445c34fec3bef8d6731d6fbec40e417
wwlodar <58707277+wwlodar@users.noreply.github.com>
2021-02-05 23:27:23 +0100

    l

--- app/models.py
+++ app/models.py
@@ -2,19 +2,30 @@ from collections import Counter
 import pandas as pd
 from heapq import nlargest
 
+
 def number_of_pokemon(df1, region1):
     return(df1.loc[df1["Region of Origin"] == region1].count()[0])
 
 def biggest_pokemon_group(df1,region1):
     df1 = df1.loc[df1["Region of Origin"] == region1]
     dict_pokemon = df1["Primary Type"].value_counts().to_dict()
-    three_largest = nlargest(3, dict_pokemon.items(), key=lambda x: x[1])
+    three_largest = nlargest(1, dict_pokemon.items(), key=lambda x: x[1])
     return list(three_largest)
+
+def worst_pokemon(df1,region1):
+    df1 = df1.loc[df1["Region of Origin"] == region1]
+    df1.sort_values(by="Base Stat Total", inplace=True)
+    df1 = df1.rename(columns={"Primary Type": "Type"})
+    df1 = df1.rename(columns={"Base Stat Total":"Combat Power"})
+    df2= df1[["Pokemon Name", "Type", "Combat Power"]]
+    df2 = df2.iloc[0:4, 0:3]
+    return df2
 
 def best_pokemon(df1,region1):
     df1 = df1.loc[df1["Region of Origin"] == region1]
-    maxClm = df1.max().["Base Stat Total"]
-    print(maxClm)
-
-
-
+    df1.sort_values(by="Base Stat Total", inplace=True, ascending=False)
+    df1 = df1.rename(columns={"Primary Type": "Type"})
+    df1 = df1.rename(columns={"Base Stat Total":"Combat Power"})
+    df2= df1[["Pokemon Name", "Type", "Combat Power"]]
+    df2 = df2.iloc[0:4, 0:3]
+    return df2
--- app/views.py
+++ app/views.py
@@ -17,9 +17,12 @@ def regionals():
     evolution_data = regional_plots.pokemon_evolution_ratio(df, region)
     pokemon_number = models.number_of_pokemon(df,region)
     biggest_group = models.biggest_pokemon_group(df,region)
+    best = models.best_pokemon(df,region)
+    worst = models.worst_pokemon(df, region)
+    titles = ' '
     return render_template('regional.html', data=data, region=region, CP_data=CP_data, evolution_data=evolution_data,
-                           pokemon_number=pokemon_number, biggest_group=biggest_group)
-
+                           pokemon_number=pokemon_number, biggest_group=biggest_group,
+                           tables=[best.to_html(index=False)], table2=[worst.to_html(index=False)], titles=titles)
 
 @app.errorhandler(404)
 def page_not_found(e):
